03955c06238061b2c2423dc7b9113717bbb9212a
刘国梁 <70141664+liu135800@users.noreply.github.com>
2020-12-04 08:46:23 +0800

    Update Gear.js

--- Gear.js
+++ Gear.js
@@ -64,6 +64,32 @@ obj = {
     "original_purchase_date_ms": "1500000000000",
     "original_purchase_date_pst": "2020-01-01 01:01:01 America/Los_Angeles",
     "original_application_version": "1.8.12.0",
+# Gear解锁订阅
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body Gear.js
+*/
+
+let obj = JSON.parse($response.body);
+obj = {
+  "status": 0,
+  "environment": "Production",
+  "receipt": {
+    "receipt_type": "Production",
+    "adam_id": 1000000000,
+    "app_item_id": 1000000000,
+    "bundle_id": "com.gear.app",
+    "application_version": "2.0.8.0",
+    "download_id": 60000000000000,
+    "version_external_identifier": 800000000,
+    "receipt_creation_date": "2100-01-01 01:01:01 Etc/GMT",
+    "receipt_creation_date_ms": "4102489858000",
+    "receipt_creation_date_pst": "2100-01-01 01:01:01 America/Los_Angeles",
+    "request_date": "2100-01-01 01:01:01 Etc/GMT",
+    "request_date_ms": "1590000000000",
+    "request_date_pst": "2020-06-05 01:01:01 America/Los_Angeles",
+    "original_purchase_date": "2020-04-18 08:31:54 Etc/GMT",
+    "original_purchase_date_ms": "1500000000000",
+    "original_purchase_date_pst": "2020-01-01 01:01:01 America/Los_Angeles",
+    "original_application_version": "1.8.12.0",
     "in_app": [{
       "quantity": "1",
       "product_id": "com.gear.app.yearly",
